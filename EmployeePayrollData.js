faf2bcc11ec2b683ac3b48247bc44f2d3590dbc3
akshat1032 <akshat.30oct@gmail.com>
2020-11-10 22:31:31 +0530

    Modified : Employee payroll extended to add new fields modified

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,27 +1,36 @@
+//  UC_13 Extend employee payroll to add gender and start date
 class EmployeePayroll {
     //property
     id;
     salary;
+    gender;
+    startDate;
 
-
-    //constructor
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-
+   //constructor
+   constructor(...params) {
+    this.id = params[0];
+    this.name = params[1];
+    this.salary = params[2];
+    this.gender = params[3];
+    this.startDate = params[4];
+}
     //getter and setter method
     get name() { return this._name; }
     set name(name) { this._name = name; }
 
-    //method 
-    toString() {
-        return "id =" + this.id + ",name = " + this.name + ",salary = " + this.salary;
-    }
+   //method 
+   toString() {
+    const options = {
+        year: 'numeric', month: 'long', day: 'numeric'
+    };
+    const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-IN", options);
+    return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", start date = " + empDate;
+}
 }
 
 let employeePayRollData = new EmployeePayRollData(1, "Mark", 30000);
 console.log(employeePayRollData.toString());
 employeePayRollData.name = "John";
 console.log(employeePayRollData.toString());
+let newEmployeePayRollData = new EmployeePayRollData(1, "Terissa", 30000, "F", new Date());
+console.log(newEmployeePayRollData.toString());
